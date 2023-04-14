d5088169a785d82fbc6b399e9ab364c9fb510077
Scorpion-5689 <ankurnagar20121@acropolis.in>
2023-04-14 15:17:46 +0530

    minor2frontend

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+	<header>
+		<h1>Agricultural Rental Equipment System</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#">Rentals</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section>
+			<h2>About Us</h2>
+			<p>The Agricultural Rental Equipment System is a platform for renting agricultural equipment. We provide farmers with a convenient and affordable way to rent the equipment they need for their farming operations.</p>
+			<p>Our mission is to help farmers increase their productivity and profitability by providing them with access to high-quality equipment at a reasonable price.</p>
+		</section>
+		<section>
+			<h2>Our Team</h2>
+			<ul class="team">
+				<li>
+					<img src="mypic.jpg" alt="John Smith">
+					<h3>Ankur Nagar</h3>
+					<p>Front-End Developer/Graphic Designer & Content Creator</p>
+				</li>
+				<li>
+					<img src="abhayimg.jpg" alt="Jane Doe">
+					<h3>Abhay Gour</h3>
+					<p>Back-End Developer & Technical Writer</p>
+				</li>
+				<li>
+					<img src="aayushi.jpg" alt="Mike Johnson">
+					<h3>Aayushi Jain</h3>
+					<p>Front-End Developer & Technical Writer</p>
+				</li>
+                <li>
+					<img src="aanya.jpg" alt="Mike Johnson">
+					<h3>Aanya Chourasiya</h3>
+					<p>Technical Writer</p>
+				</li>
+			</ul>
+		</section>
+	</main>
+	<footer>
+		<p>&copy; 2023 Agricultural Rental Equipment System</p>
+	</footer>
+</body>
+</html>
